ea207c86ad3e96acc1b093b2e24423120e37336d
KareemjWall-Sanders <kareemjwallssanders@gmail.com>
2019-03-13 23:26:49 -0400

    first commit

--- morningchallenge#2/js/match.js
+++ morningchallenge#2/js/match.js
@@ -1,10 +1,18 @@
 // Build a card game that allows a user to macth cards.  
 // The game will start with 10 cards for the user to choose from
-// and will alret the amount of times the user takes to find 5 sets
-// of matches needed to complete the game.
+// and will alret the amount of times the user takes to find all 5 matches
+
+//=======Variables==========//
+
+// const cardValues = ["0","1","2","3","4","0","1","2","3","4"];
+let cardOne = document.getElementsByClassName('frontCard');
+let cardTwo = document.getElementsByClassName('backCard');
+
+//======spred snytax====//
+let card = [...gameCard]
+//======================//
 
 
-//================pusdeo code===============//
 
 // The user will choose 2 desired card by click, upon the click the cards turns over.
 // After the user chooses the desired card then AI will check to see if the card match.
@@ -18,114 +26,116 @@
 //4.When creating the cards, the pairs will have the same classes(5)
 
 
-window.addEventListener('load', function(){
-  // console.log("this works");
-  var cardValues = ["0","1","2","3","4","0","1","2","3","4"];
-  cardValues.forEach(function(el){
+//==========================//
+
+
+
+function shuffle(arry) {
+  let fisrtIndex
+}
+
+
+//==============================Psudoe Code=================================//
+
+// The memory match game starts off with 
+// 10 cards lying backside up, 
+//a user then chooes a card by clicking on it, next the card 
+document.querySelectorAll('memory-game').addEventListener('click', pickCard)
+
+
+function pickCard() {
+
+  // turns face up and waits until a second card is choosen.
+
+  // var checkForMatch = function() {
+  //     if (cardsInPlay[0] === cardsInPlay[1]) {
+  //           console.log("You found a match!");
+  //         } else {
+  //             console.log("Sorry, try again.");
+  //         }
+  // }
+
+  // var flipCard = function(cardId) {
+
+  //     document.getElementById('q').src=faces[cardId]
+
+  //     checkForMatch();
+  // }
+
+
+}
+
+
+
+
+// Upon the player second choice by clicking on the second card.
+// the application then checks to see if the card choosen match,
+// and if the cards match, the cards stay flipped over, 
+// and if cards do not match the card then flip bact to backside up.
+// and the user repeats the process.unitl the user finds all 5 pairs,
+// then the game restarts after click of a button.
+
+
+// ==========================================================================//
+
+
+
+
+document.body.onload = startgame();
+start / reset
+
+function startReset() {
+
+}
+
+function
+
+//checking conditons
+
+for (let i = 0; i <= cards.length; i++){
+
+console.log();
+}
+
+  // // }
+
+  // // // ==============flipping cards==========//
+function flipCard() {
+  const top1 = document.querySelector("#top1")
+  const top2 = document.querySelector("#top2")
+  const top3 = document.querySelector("#top3")
+  const mid1 = document.querySelector("#mid1")
+  const mid2 = document.querySelector("#mid2")
+  const mid3 = document.querySelector("#mid3")
+  const bot1 = document.querySelector("#bot1")
+  const bot2 = document.querySelector("#bot2")
+  const bot3 = document.querySelector("#bot3")
+  const bot3 = document.querySelector("#bot3")
+
+
+}
+class Jordan {
+  this.image = Jordan;
+  this.flipped = false;
+}
+
+for(var i = 0; i< cards.length; i++)
+card = cards[i];
+
+document.getElementsByClassName('memory-game').addEventListener('click', )
+document.getElementsByClassName('memory-game').addEventListener('click', )
+
+
+window.addEventListener('load', function () {
+  console.log("this works");
+  cardValues.forEach(function (el) {
     var card = document.createElement("section")
     card.innerHTML = "";
     card.className = "card";
     document.body.appendChild(card);
     console.log(card);
-    card.addEventListener("click", function(e){
+    card.addEventListener("click", function (e) {
       console.log("hi");
     })
   })
 })
-
-
-// document.getElementById('flip').addEventListener( 'click', function(){
-//     card.toggleClassName('flipped');
-//   }, false);
-
-
-  
-// var faces = []; //array to store card images
-//      faces[0] = 'images/king-of-diamonds.png';
-//      faces[1] = 'images/king-of-hearts.png';
-//      faces[2] = 'images/queen-of-diamonds.png';
-//      faces[3] = 'images/queen-of-hearts.png';
-
-// var cardsInPlay = [];
-
-// var checkForMatch = function() {
-//     if (cardsInPlay[0] === cardsInPlay[1]) {
-//           console.log("You found a match!");
-//         } else {
-//             console.log("Sorry, try again.");
-//         }
-// }
-
-// var flipCard = function(cardId) {
-
-//     document.getElementById('q').src=faces[cardId]
-
-//     checkForMatch();
-// }
-
-
-
-
-
-
- 
-
-
-
-  
-
-
-// // // start/reset
-// // function startReset(){
-
-// // }
-
-// // function 
-
-// // //checking conditons
-// // function(){
-// //     for (let =0; i<=; i++)
-    
-// // }
-
-// // // ==============flipping cards==========//
-// // function flipping(){
-// //     const top1 = document.querySelector("#top1")
-// //     const top2 = document.querySelector("#top2")
-// //     const top3 = document.querySelector("#top3")
-// //     const mid1 = document.querySelector("#mid1")
-// //     const mid2 = document.querySelector("#mid2")
-// //     const mid3 = document.querySelector("#mid3")
-// //     const bot1 = document.querySelector("#bot1")
-// //     const bot2 = document.querySelector("#bot2")
-// //     const bot3 = document.querySelector("#bot3")
-    
-// // }
-// // class Jordan{
-// //     this.image = Jordan;
-// //     this.flipped = false;
-// // }
-
-// // const = new Jordan
-
-// // class Bird{
-// //     this.image = Bird;
-// //     this.flipped = false;
-// // }
-
-// // class Magic{
-// //     this.image = Magic;
-// //     this.flipped = false;
-    
-// // }
-
-// // class DrJ {
-// //     this.image = DrJ
-// //     this.flipped = false;
-// // }
-// // class Kareem{
-// //     this.image = Kareem;
-// //     this.flipped = false;
-// // }
-
-
